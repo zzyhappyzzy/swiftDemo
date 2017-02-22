2cbcfae5abc80546f33ec8c8c51b24e64be2ffed
zhenzhaoyang <zzyhappyzzy@163.com>
2017-02-22 11:37:44 +0800

    update code

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '7.0'
+
+target 'SwiftDemo' do
+    pod "FDStackView", "1.0"
+end
--- SwiftDemo/ViewController.swift
+++ SwiftDemo/ViewController.swift
@@ -10,16 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    let elementList: [String] = ["Carbon", "Gold", "Chlorine", "Sodium"]
+    var currentElementIndex = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateElement()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func updateElement() {
+        answerLabel.text = "?"
+        let elementName = elementList[currentElementIndex]
+        let image = UIImage(named: elementName)
+        imageView.image = image
+    }
 
-
+    @IBAction func showAnswer(_ sender: Any) {
+        answerLabel.text = elementList[currentElementIndex]
+    }
+    
+    @IBAction func gotoNextElement(_ sender: Any) {
+        currentElementIndex += 1
+        if currentElementIndex >= elementList.count {
+            currentElementIndex = 0
+        }
+        updateElement()
+    }
+    
 }
 
